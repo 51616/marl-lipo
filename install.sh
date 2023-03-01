31947f956c2175f4f91b1e03b2dea064a8f6e1ef
Rujikorn Charakorn <rujikorn.c@outlook.com>
2023-03-01 18:00:03 +0700

    Update install.sh
    add rebar clone

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ pip install --upgrade pip
 pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r requirements.txt
 # install rebar
-cd coop_marl/utils/rebar
+cd coop_marl/utils/
+git clone https://github.com/51616/rebar.git
+cd rebar
 python setup.py develop
 cd ../../..
 
