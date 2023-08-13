45aeb044a6c401f58ab8f39ae2b50c7b24ead72d
Rujikorn Charakorn <rujikorn.c@outlook.com>
2023-08-13 17:19:35 +0200

    downgrade protobuf

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ pip install --upgrade pip
 pip install setuptools==66
 pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r requirements.txt
+pip install protobuf==3.20
 # install rebar
 cd coop_marl/utils/
 git clone https://github.com/51616/rebar.git
