df9a3f28fb0f25e5136947756e80ccdea365a2d4
Rujikorn Charakorn <rujikorn.c@outlook.com>
2024-05-10 18:27:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@ sudo apt-get install gifsicle xvfb -y
 conda install -c conda-forge cudatoolkit=11.1 cudnn=8.4.1
 pip install --upgrade pip
 pip install setuptools==66
+pip install wheel==0.38.4
 pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r requirements.txt
 pip install protobuf==3.20
+
 # install rebar
 cd coop_marl/utils/
 git clone https://github.com/51616/rebar.git
